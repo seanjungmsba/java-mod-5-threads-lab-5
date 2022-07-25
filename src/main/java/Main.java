898232ccbca1e48b9e2d619fd890a3a1aa45c87a
seanjungmsba <seanjeong1996@gmail.com>
2022-07-25 19:36:20 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        // your code here
+    public static void main(String[] args) throws InterruptedException {
+
+        SavingsAccount account1 = new SavingsAccount("Example-Account-1");
+        SavingsAccount account2 = new SavingsAccount("Example-Account-2");
+
+        new Thread(account1::printThread).start(); // Thread-0
+        new Thread(account2::printThread).start(); // Thread-1
+
     }
 }
--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,16 +1,50 @@
-public class SavingsAccount {
+public class SavingsAccount implements Runnable {
 
-  private long total = 0;
+    private long total = 0;
+    private final String name;
 
-  public boolean withdraw(long amount) {
-      
-  }
+    public SavingsAccount(String name) {
+        this.name = name;
+        this.total = 0;
+    }
 
-  public void deposit(long amount) {
+    public boolean withdraw(long amount) {
+        if (amount <= this.total) {
+            this.total -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-  }
+    public void deposit(long amount) {
+        this.total += amount;
+    }
 
-  public long getTotal() {
-      return total;
-  }
+    public long getTotal() {
+        return this.total;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void run() {
+        String curThreadName = Thread.currentThread().getName();
+        System.out.println(curThreadName + " before depositing and withdrawing = " + total);
+        this.deposit(50_000);
+        this.withdraw(30_000);
+        System.out.println(curThreadName + " after depositing and withdrawing = " + total);
+    }
+
+    public synchronized void printThread() {
+        String curThreadName = Thread.currentThread().getName();
+        System.out.println(curThreadName + " entering printThread method of " + name + " with balance of $" + total);
+        System.out.println("STATUS: Depositing $50,000");
+        this.deposit(50_000);
+        System.out.println("STATUS: Withdrawing $20,000");
+        this.withdraw(20000);
+        System.out.println(curThreadName + " exiting printThread method of " + name  + " with balance of $" + total);
+    }
 }
